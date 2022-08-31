6bb72e90ea4250d02c6ecf0c85c251fb74a06f01
JacobGreen-git <jacoblgreen96@gmail.com>
2022-08-31 12:00:19 -0500

    Classes

---
+++ OOP/OOP_Four_Pillars.py
@@ -0,0 +1,30 @@
+#OOP - The Four Pillars
+
+
+#Encapsulation
+    #Grouping code together into these entities we call classes
+    
+
+
+#Inheritance
+    #ability to create subclasses that have all the attributes and methods of the parent class 
+    #key word super() will access the parent class
+
+
+#Polymorphism
+    #Child classes can have different difinition than the parent if the values are overwritten
+
+#Adstraction
+    #Abstraction is an extension of Encapsulation, and we can hide attributes or methods that an 
+
+
+#####
+
+#Packages + modules
+#Modules are just .py files that hold functionality -- they might be made up of function, classes, variable, etc.
+#Packages -- are dictionaries that hold multiple modules
+
+#####
+
+#input()
+#a function to get info from the user
