83d40011a1beaa4f72ca441decc3ee0f9ed061c3
JacobGreen-git <jacoblgreen96@gmail.com>
2022-08-31 16:08:21 -0500

    Pirate Game

--- OOP/game.py
+++ OOP/game.py
@@ -6,9 +6,9 @@ Veigar = mage.mage('Fire Ball')
 
 while(bob.health > 0 and Veigar > 0):
     response = input("You're Bob, will you attack or defend? \n 1) Attack \n 2) Defend)")
-    if response == '1'
+    if response == '1':
         bob.attack(Veigar)
-    elif response == '2'
+    elif response == '2':
         bob.defend()
     else:
         print("Please pick a valid option")
