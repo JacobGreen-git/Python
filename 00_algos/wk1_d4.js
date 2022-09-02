8f24c27e7f0e60687f16704c69b4544403de6bf7
JacobGreen-git <jacoblgreen96@gmail.com>
2022-09-02 10:40:35 -0500

    PM lect

---
+++ 00_algos/wk1_d4.js
@@ -0,0 +1,100 @@
+/* 
+Given in an alumni interview in 2021.
+String Encode
+You are given a string that may contain sequences of consecutive characters.
+Create a function to shorten a string by including the character,
+then the number of times it appears. 
+
+If final result is not shorter (such as "bb" => "b2" ),
+return the original string.
+  */
+
+const str1 = "aaaabbcddd";
+const expected1 = "a4bbcd3";
+const expected1b = "a4b2cd3";
+
+const str2 = "";
+const expected2 = "";
+
+const str3 = "a";
+const expected3 = "a";
+
+const str4 = "bbcc";
+const expected4 = "bbcc";
+
+const str5 = "abbbbbbbbbbbbbbbbb"
+const expected5 = "ab17"
+
+
+function encodeStr(str) {
+  let encoded = "";
+
+  for (let i = 0; i < str.length; i++) {
+      let currChar = str[i];
+      let dupeCount = 1;
+
+      while (str[i + 1] === currChar) {
+          dupeCount++;
+          i++;
+      }
+
+      if (dupeCount === 1) {
+          encoded += currChar
+      }
+      else if (dupeCount === 2) {
+          encoded += currChar + currChar
+      }
+      else {
+          encoded += currChar + dupeCount;
+      }
+  }
+  return encoded;
+}
+
+
+// function encodeStr(str) {
+//     let newStr = ''
+//     // let arr =''
+//     let letterCount = 0
+//     for(let i=0; i<=str.length-1; i++){
+//         // arr
+//         if(str[i] == str[i+1]){
+//             letterCount += 1
+//         }
+//         // newStr += 
+//         newStr += letterCount
+        
+//     }
+// }
+
+  console.log(encodeStr(str1)) // Expected: a4bbcd3
+  console.log(encodeStr(str2)) // Expected: ""
+  console.log(encodeStr(str3)) // Expected: a
+  console.log(encodeStr(str4)) // Expected: bbcc
+  console.log(encodeStr(str5)) // Expected: ab17
+
+
+
+
+const strA = "a3b2c1d3";
+const expectedA = "aaabbcddd";
+
+const strB = "a3b2c12d10";
+const expectedB = "aaabbccccccccccccdddddddddd";
+
+/**
+   * Decodes the given string by duplicating each character by the following int
+   * amount if there is an int after the character.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {string} str An encoded string with characters that may have an int
+   *    after indicating how many times the character occurs.
+   * @returns {string} The given str decoded / expanded.
+   */
+  //helpful built-ins : isNaN() .repeat() parseInt() 
+function decodeStr(str) {
+      //Your code here
+}
+
+//   console.log(decodeStr(strA)) // Expected: aaabbcddd
+//   console.log(decodeStr(strB)) // Expected: aaabbccccccccccccdddddddddd
