32a645465d608d356192cae44bce1992679277bf
JacobGreen-git <jacoblgreen96@gmail.com>
2022-09-14 15:13:55 -0500

    Reg and Log

---
+++ MySQL/Login_and_Reg/flask_app/controllers/user_controller.py
@@ -0,0 +1,51 @@
+import re
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_app.models.user_model import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app) 
+
+@app.route('/')
+def index():
+    return redirect('/home')
+
+@app.route('/home')
+def home():
+    return render_template('login.html')
+
+@app.route('/user/register', methods=["POST"])
+def register():
+    if not User.validate(request.form):
+        return redirect ('/home')
+    hashed_pass = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        **request.form,
+        'password': hashed_pass
+    }
+    id = User.create(data)
+    session['user_id'] = id
+    return redirect ('/welcome')
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    data = {'email':request.form['email']}
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash('Invalid login info', 'log')
+        return redirect ('/home')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash('Invalid login info', 'log')
+        return redirect ('/home')
+    session['user_id'] = user_in_db.id
+    return redirect('/welcome')
+
+@app.route('/welcome')
+def welcome():
+    if 'user_id' not in session:
+        return redirect('/')
+    return render_template('welcome.html')
+
+@app.route('/logout')
+def logout():
+    del session['user_id']
+    return redirect ('/')
---
+++ MySQL/dbs/login_and_Reg_db.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema Login_and_Reg_db
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Login_and_Reg_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Login_and_Reg_db` DEFAULT CHARACTER SET utf8 ;
+USE `Login_and_Reg_db` ;
+
+-- -----------------------------------------------------
+-- Table `Login_and_Reg_db`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Login_and_Reg_db`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(60) NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
