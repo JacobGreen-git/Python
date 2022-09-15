724e3b2cdf4afb5a1a386bc73f70802558387039
JacobGreen-git <jacoblgreen96@gmail.com>
2022-09-14 22:54:15 -0500

    Prep

--- MySQL/belt_exam/flask_app/controllers/user_controller.py
+++ MySQL/belt_exam/flask_app/controllers/user_controller.py
@@ -1,9 +1,8 @@
 import re
 from flask import render_template, redirect, request, session, flash
 from flask_app import app
-from flask_app.models.user_model import User
-from flask_app.models.recipe_model import Recipe
-from flask_app.controllers import recipe_controller
+from flask_app.models import user_model
+
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app) 
 
@@ -17,14 +16,14 @@ def home():
 
 @app.route('/user/register', methods=["POST"])
 def register():
-    if not User.validate(request.form):
+    if not user_model.User.validate(request.form):
         return redirect ('/home')
     hashed_pass = bcrypt.generate_password_hash(request.form['password'])
     data = {
         **request.form,
         'password': hashed_pass
     }
-    id = User.create_user(data)
+    id = user_model.User.create_user(data)
     print(id)
     session['user_id'] = id
     return redirect ('/welcome')
@@ -32,7 +31,7 @@ def register():
 @app.route('/user/login', methods=['POST'])
 def login():
     data = {'email':request.form['email']}
-    user_in_db = User.get_by_email(data)
+    user_in_db = user_model.User.get_by_email(data)
     if not user_in_db:
         flash('Invalid login info', 'log')
         return redirect ('/home')
@@ -46,4 +45,3 @@ def login():
 def logout():
     del session['user_id']
     return redirect ('/')
-
---
+++ MySQL/belt_exam/flask_app/models/user_model.py
@@ -0,0 +1,129 @@
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+# from flask_app.models import 
+#IMPORT OTHER MODEL HERE
+
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+mysql_db = "recipe_db"
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    #CREATE
+    @classmethod
+    def create_user(cls, data):
+        query ='INSERT INTO users (first_name, last_name, email, password) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s);'
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        return results
+
+    #READ ALL
+    @classmethod
+    def get_all_users(cls):
+        query = 'SELECT * FROM users;'
+        results = connectToMySQL(mysql_db).query_db(query)
+        print("============================")
+        print("This is the result we got from our get all query...", results)
+        print("============================")
+        empty_list = []
+        for u in results:
+            empty_list.append(cls(u))
+        print("============================")
+        print("This is our list of users...", empty_list)
+        print("============================")
+        return empty_list
+    
+    #READ ONE
+    @classmethod
+    def get_one_user(cls,data):
+        query = 'SELECT * FROM users WHERE id=%(id)s;'
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        the_one_item = cls(results[0])
+        return the_one_item
+    
+    #UPDATE
+    @classmethod
+    def update_user(cls, data):
+        query = 'UPDATE users SET COLUMN_NAME=%(FORM_NAME)s WHERE id=%(id)s;'
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        return results
+
+    #DELETE
+    @classmethod
+    def destroy_user(cls, data):
+        query = 'DELETE FROM users WHERE id = %(id)s;'
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        return results
+
+    #GET BY ID WITH JOIN
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users LEFT JOIN recipes on users.id = recipes.user_id WHERE users.id = %(id)s;"
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        user = cls(results[0])
+        list_of_recipes = []
+        for row in results:
+            if row['recipes.id'] == None:
+                break
+            recipe_data = {
+                **row,
+                'id': row['recipes.id'],
+                'created_at': row['recipes.created_at'],
+                'updated_at': row['recipes.updated_at']
+            }
+            this_recipe = recipe_model.Recipe(recipe_data)
+            list_of_recipes.append(this_recipe)
+        user.recipes = list_of_recipes
+        return user
+
+    #GET BY MAIL TO VALIDATE
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(mysql_db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    #VALIDATE
+    @staticmethod
+    def validate(user_data):
+        is_valid = True
+        if len(user_data['first_name']) < 2:
+            flash("First name required", "reg")
+            is_valid = False
+        if len(user_data['last_name']) < 2:
+            flash("Last name required", "reg")
+            is_valid = False
+        if len(user_data['email']) < 2:
+            flash("Email required", 'reg')
+            is_valid = False
+        if len(user_data['password']) < 8:
+            flash("Password must be at least 8 characters", "reg")
+            is_valid = False
+        elif not user_data['password'] == user_data['confirm_pass']:
+            flash("Passwords do not match", "reg")
+            is_valid = False
+        elif not EMAIL_REGEX.match(user_data['email']):
+            flash("Invalid Email", "reg")
+            is_valid=False
+        else:
+            data = {
+                'email':user_data['email']
+            }
+            potential_user = User.get_by_email(data)
+            if potential_user:
+                flash("Email already in use", 'reg')
+                is_valid = False
+        return is_valid
