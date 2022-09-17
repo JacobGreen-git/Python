b9346e06e7c7ad7b3bdbae63da448d1559f4cb47
JacobGreen-git <jacoblgreen96@gmail.com>
2022-09-16 20:03:16 -0500

    wk3_d5

--- MySQL/Recipes/flask_app/controllers/recipe_controller.py
+++ MySQL/Recipes/flask_app/controllers/recipe_controller.py
@@ -1,4 +1,3 @@
-from tkinter.messagebox import RETRY
 from flask import render_template, redirect, request, session, flash
 from flask_app import app
 from flask_app.models.recipe_model import Recipe
